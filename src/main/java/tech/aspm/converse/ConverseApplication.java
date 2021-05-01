4a2b5e966cada181438a7536a4db5df09bc625ae
Ashkan Pourghasem <ashkan.pourghasem@googlemail.com>
2021-05-01 15:12:01 +0400

    Init Spring Boot

---
+++ src/main/java/tech/aspm/converse/ConverseApplication.java
@@ -0,0 +1,13 @@
+package tech.aspm.converse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConverseApplication.class, args);
+	}
+
+}
